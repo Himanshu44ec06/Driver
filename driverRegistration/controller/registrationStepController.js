2bec0bcc33a228f3794e29cf7ba7ce5e8220198a
Himanshu44ec06 <himanshu.44ec06@gmail.com>
2018-03-08 21:46:40 +0530

    Move check  step  Logic from  test to controller

--- driverRegistration/controller/registrationStepController.js
+++ driverRegistration/controller/registrationStepController.js
@@ -1,12 +1,11 @@
-// @ts-check
 'use strict'
 //  use to define  registration step  Dal
 const  RegistrationStep =  require('../model/registrationsteps');
-
+const  calculation =  require('../model/Calculation');
 
 const  RegistrationStepController  =  (function(){
 
-    var controller =  {};
+var controller =  {};
 
     //  Currently  not in  use 
     controller.AddNew =  function(registrationStep){
@@ -19,6 +18,44 @@ const  RegistrationStepController  =  (function(){
 
     controller.GetByName =  function (stepname) {
          return  RegistrationStep.findOne({StepName :  stepname});
+    }
+
+
+    controller.RunStep =  function (stepname,driver) {
+        let returnItem  = {
+            status : false,
+            message : []
+        };
+
+        return  RegistrationStep.findOne({StepName :  stepname}).then((step)=>{
+
+            if(step){
+                var  isPassed  =  true;
+                for (var i = 0, len = step.Inputs.length; i < len; i++) {
+                    var  inputItem  =  step.Inputs[i];
+                    for (var j = 0, jlen = inputItem.PassCriteria.length; j < jlen; j++) {
+                        var  criteria  =  inputItem.PassCriteria[j];
+                        var isValid  =  calculation[criteria.Function](driver[inputItem.InputName],
+                            criteria.CompareValue)
+
+                        if(!isValid){
+                            returnItem.message.push(inputItem.InputName + ' is  failed');
+                            isPassed =  false;
+                        }
+                    }
+                }
+                returnItem.status =  isPassed;
+
+            }
+            else {
+                returnItem.message.push('Step  not  found');
+            }
+
+            return Promise.resolve(returnItem);
+
+
+        });
+
     }
 
     return  controller;
--- test/test_registrationStepWithDriver.js
+++ test/test_registrationStepWithDriver.js
@@ -16,39 +16,13 @@ describe('Registration Step Verfication',()=>{
             VisaStatus :  'H1'
         };
 
-        registrationStep.GetByName(stepName).then((step)=>{
+         registrationStep.RunStep(stepName,driver).then((result)=>{
 
-                if(step){
-                        var  isPassed  =  true;
-                    for (var i = 0, len = step.Inputs.length; i < len; i++) {
-
-                        var  inputItem  =  step.Inputs[i];
-
-                        console.log('Validating ' + inputItem.InputName);
-
-                        for (var j = 0, jlen = inputItem.PassCriteria.length; j < jlen; j++) {
-                            var  criteria  =  inputItem.PassCriteria[j];
-                            var isValid  =  calculation[criteria.Function](driver[inputItem.InputName],
-                                criteria.CompareValue)
-
-                            console.log('Result of  ' + inputItem.InputName + ' -->' + isValid);
-
-                            if(!isValid){
-                                console.log(inputItem.InputName + ' -- Failed');
-                                isPassed =  false;
-                            }
-                        }
-                    }
-                        assert(isPassed);
-
-                }else {
-                    assert.fail('Step  not  Found -- FIX ME ');
-                }
-                done();
-        }).catch((e)=>{
-            assert.fail('Step  not  Found -- FIX ME ');
-            done();
+            assert(result.status);
+          done();
         });
+
+
 
     });
 
